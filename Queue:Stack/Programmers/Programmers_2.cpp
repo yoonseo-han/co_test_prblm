9ccf73e004eada54e198a90d36f63f8d00ff538d
Yoonseo Han <yooncer00@gmail.com>
2022-04-07 21:07:18 +0900

    Final answer for Queue/Stack programmers 2

--- Queue:Stack/Programmers/Programmers_2.cpp
+++ Queue:Stack/Programmers/Programmers_2.cpp
@@ -19,10 +19,12 @@ int calculate_weight(vector<int> weight, int start) {
 
 //helper function to shift values of vector
 void update_vector(vector<int> &ref) {
-    for(int i=0; i<ref.size()-1; i++) {
-        ref[i] = ref[i+1];
+    if(!ref.empty()) {
+        for(int i=0; i<ref.size()-1; i++) {
+            ref[i] = ref[i+1];
+        }
+        ref.pop_back();
     }
-    ref.pop_back();
 }
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
@@ -31,6 +33,8 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
     queue <int> store;
     //bridge queue
     queue <int> bridge;
+    //Vector to store all finished trucks
+    vector<int> finished;
     int numElements = truck_weights.size();
     //store vector in form of queue
     for(int i=0; i<numElements; i++) {
@@ -38,30 +42,37 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
     }
     int total_weight = calculate_weight(truck_weights, 0);
     //Repeate till store queue is empty
-    while(!store.empty()) {
+    while(calculate_weight(finished,0)!=total_weight) {
         int store_front = store.front();
         //get total weight on bridge = total weight - total weight in waiting array (excluding the front element)
-        int weight_on_bridge = total_weight - calculate_weight(truck_weights, 1);
+        int weight_on_bridge = total_weight - calculate_weight(truck_weights, 0) - calculate_weight(finished,0);
         //Subtract weight on bridge from the front of bridge (if bridge is not null)
-        if(!bridge.empty()) weight_on_bridge -= bridge.front();
+        if(!bridge.empty() && bridge.size()==bridge_length) weight_on_bridge -= bridge.front();
+        //Add the first element on truck_weights for weight_on_bridge
+        if(!truck_weights.empty()) weight_on_bridge += truck_weights.front();
         //Compare the updated new weight is smaller or equal to bridge max weight
         if(weight_on_bridge<=weight) {
             //If so, update the members on store queue, bridge queue, truckweights vector
-            bridge.push(store.front());
+            if(!bridge.empty() && bridge.size() == bridge_length) {
+                finished.push_back(bridge.front());
+                bridge.pop();
+            }
+            bridge.push(store_front);
             store.pop();
-            if(!bridge.empty()) bridge.pop();
             //Update vector
             update_vector(truck_weights);
         }
         else {
             //If not, leve the store queue and truck weights unchaged, 
             //Remove front on bridge and add 0 to end
-            if(!bridge.empty()) bridge.pop();
+            if(!bridge.empty() && bridge.size() == bridge_length) {
+                finished.push_back(bridge.front());
+                bridge.pop();
+            }
             bridge.push(0);
         }
         //For both cases increment time by 1
         time++;
-        cout<<total_weight<<" / "<< weight_on_bridge<< " \n ";
     }
     return time;
 }
@@ -72,7 +83,7 @@ int main() {
     vector<int> truck_weights {7,4,5,6};
 
     int time = solution(bridge_length, weight, truck_weights);
-    cout<<time;
+    cout<<"time: "<<time;
 
     return 0;
 }
