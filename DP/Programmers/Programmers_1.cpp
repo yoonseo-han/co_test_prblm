5410d7091a3d7ad56c6efa98505a7e5078797759
Yoonseo Han <yooncer00@gmail.com>
2022-04-08 15:03:10 +0900

    Removed greedy/programmers q1 compiled file

---
+++ DP/Programmers/Programmers_1.cpp
@@ -0,0 +1,22 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42895
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int N, int number) {
+    int answer = 0;
+
+    
+
+    return (answer>8)? -1 : answer;
+}
+
+int main() {
+    int N, number;
+    cin>>N>>number;
+    cout<< "Number of times: "<<solution(N,number);
+    return 0;
+}
