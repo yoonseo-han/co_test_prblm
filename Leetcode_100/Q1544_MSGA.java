272e8ac3cb69d2420cbb31624de27ef5b3e97a43
Yoonseo Han <yooncer00@gmail.com>
2022-11-09 20:49:31 +0800

    Brute force solution for Q1544

--- Leetcode_100/Q1544_MSGA.java
+++ Leetcode_100/Q1544_MSGA.java
@@ -1,27 +1,29 @@
 package Leetcode_100;
 
-import java.io.*;
-import java.util.*;
 
 class Solution {
     public static String makeGood(String s) {
-        char[] ch = s.toCharArray();
+
         int count = 0;
         while(true) {
             count = 0;
             for(int i=0; i<s.length()-1; i++) {
+                //Update the string to store as individual character arrays
+                char[] ch = s.toCharArray();
                 //If inappropriate then remove both i and i+1
-                if(ch[i]-ch[i+1]==32) {
-
+                if(ch[i]-ch[i+1]==32 ||ch[i]-ch[i+1]==-32 ) {
+                    //Erase the following i, i+1 from the original string
+                    s = s.substring(0, i) + s.substring(i+2);
+                    //Increment count
+                    count++;
                 }
             }
             if(count == 0) {
                 break;
             }
+            //s = s.substring(0, 1);
         }
-
-        String newS = s.substring(0, 1) + s.substring(4);
-        return newS;
+        return s;
     }
     public static void main(String[] args){
         String output = makeGood("leEeetcode");
