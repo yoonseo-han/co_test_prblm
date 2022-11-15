b64ee726d949b5239a64e16cc247f357a2d371fe
Yoonseo Han <yooncer00@gmail.com>
2022-11-15 12:51:47 +0800

    First commit in Q13 Leetcode

--- Leetcode_100/Q13_Roman_to_integer.java
+++ Leetcode_100/Q13_Roman_to_integer.java
@@ -1,23 +1,35 @@
 package Leetcode_100;
 
+import java.util.HashMap;
+
 class Solution {
-    public static int[] twoSum(int[] nums, int target) {
-        int [] answer={1,1};
-        for(int i=0; i<nums.length-1; i++) {
-            int find = target - nums[i];
-            for(int j=i+1; j<nums.length; j++) {
-                if(nums[j]==find) {
-                    answer[0] = i;
-                    answer[1] = j;
-                    return answer;
-                }
+    public static int romanToInt(String s) {
+        //Define hashmap of the key values
+        HashMap <Character, Integer> Dictionary = new HashMap<>();
+
+        //Add Roman numerals and there corresponding values
+        Dictionary.put('I', 1);
+        Dictionary.put('V', 5);
+        Dictionary.put('X', 10);
+        Dictionary.put('L', 50);
+        Dictionary.put('C', 100);
+        Dictionary.put('D', 500);
+        Dictionary.put('M', 1000);
+
+        //Convert input string to character
+        char[] ch = s.toCharArray();
+        for(int i=0; i<s.length(); i++){
+            //Check if character exists in dictionary
+            if(Dictionary.containsKey(ch[i])) {
+                //Retrieve the following value of the corresponding key
+                
             }
         }
-        return answer;
+        return 0;
     }
     public static void main(String[] args){
-        int[] nums = {3,2,4};
-        int[] output = twoSum(nums,6);
-        System.out.println(output[0]);
+        int output = romanToInt("III");
+        System.out.println(output);
     }
 }
+
