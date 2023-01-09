9cdcbd3ba9563e9a251880c559b87ccefbd15888
Yoonseo Han <yooncer00@gmail.com>
2023-01-09 18:09:24 +0900

    Solvd Q136 Leetcode

--- Leetcode_100/Q136_Single_Num.java
+++ Leetcode_100/Q136_Single_Num.java
@@ -4,12 +4,21 @@ import java.util.Objects;
 
 class Solution {
     public static int singleNumber(int[] nums) {
-        int[][] temp = new int[3*10^4][2];
+        //Find max element number
+        int max=0;
+        for(int i=0; i<nums.length; i++) {
+            int value = (nums[i] >0) ? (nums[i]) : (nums[i]*(-1));
+            if(max<value) max=value;
+        }
+
+        //Define counter array size based on max value of input array
+        int[][] temp = new int[max+1][2];
         //Initialize counter function
         for(int i=0; i<temp.length; i++) {
             temp[i][0] = 0;
             temp[i][1] = 0;
         }
+
         //Increase count if positive number, decrease if negative
         for(int i=0; i<nums.length; i++) {
             int index = (nums[i] >0) ? (nums[i]) : (nums[i]*(-1));
