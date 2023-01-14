3b2527a33dff5d7aa6e40dd65ecb3a6727ce6155
Yoonseo Han <yooncer00@gmail.com>
2023-01-14 22:32:13 +0900

    Leetcode Q190 comment

--- Leetcode_100/Q190_Reverse_bits.java
+++ Leetcode_100/Q190_Reverse_bits.java
@@ -7,9 +7,12 @@ class Solution {
     public static int reverseBits(int n) {
         int result = 0;
         for (int i = 0; i < 32; i++) {
+            //Retrieve the right end of the input integer : Note that 1 = 000...001
             result += n & 1;
+            //Shift the input 1 position to right as rightmost element already dealt
             n >>>= 1;   //  must do unsigned shift
             if (i < 31) //  for last digit, don't shift!
+                //As for the result, shift left as need to store in reverse order
                 result <<= 1;
         }
         return result;
