71eb835ced23980193590bb36b788ffc2e491d1f
Yoonseo Han <yooncer00@gmail.com>
2023-01-29 19:09:20 +0900

    Try recursive approach for Q33

---
+++ Leetcode_100/Q33_Recursive_approach.java
@@ -0,0 +1,17 @@
+package Leetcode_100;
+
+class Solution {
+    public static int search(int [] nums, int target){
+        int start = 0;
+        int end = nums.length -1;
+
+        //USe recursive approach
+
+        return -1;
+    }
+    public static void main(String strs[]) {
+        int[] inputArray = {4,5,6,7,1,2};
+        int target = 5;  
+        System.out.println(search(inputArray, target));
+    }
+}
---
+++ Leetcode_100/Q34_First_and_last_position.java
@@ -0,0 +1,11 @@
+package Leetcode_100;
+
+class Solution {
+    public static int[] searchRange(int[] nums, int target) {
+        return nums;
+    }
+
+    public static void main(String strs[]) {
+        
+    }
+}
