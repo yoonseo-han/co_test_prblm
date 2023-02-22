57e4819d28c93658dcfb05603983b43beddfd9a2
Yoonseo Han <yooncer00@gmail.com>
2023-02-22 12:36:11 +0800

    Change file structure

---
+++ Bruteforce/PG_Find_Prime.java
@@ -0,0 +1,17 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java
+
+package Bruteforce;
+
+class Solution {
+    public static int solution(String numbers) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+    public static void main(String strs[]) {
+
+        
+    }
+}
