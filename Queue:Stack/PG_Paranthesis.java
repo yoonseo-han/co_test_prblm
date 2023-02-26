b06d353f202613ee5aa1dd7e4a0adacc97391d0c
Yoonseo Han <yooncer00@gmail.com>
2023-02-26 18:13:53 +0800

    PG stack

---
+++ Queue:Stack/PG_Paranthesis.java
@@ -0,0 +1,15 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+package Queue:Stack;
+
+class Solution {
+    public static boolean solution(String s) {
+        boolean answer = true;
+
+        return answer;
+    }
+
+    public static void main(String strs[]) {
+        
+    }
+}
