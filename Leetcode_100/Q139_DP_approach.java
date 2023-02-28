bf0a61f8fac6d66eec6567e599579eab58ffe234
Yoonseo Han <yooncer00@gmail.com>
2023-02-28 17:24:55 +0800

    Leetcode Q139 DP Approach

---
+++ Leetcode_100/Q139_DP_approach.java
@@ -0,0 +1,28 @@
+package Leetcode_100;
+
+import java.util.*;
+
+class Solution {
+    public boolean wordBreak(String s, List<String> dictionary) {
+        Set<String> dict = new HashSet<>(dictionary);
+        if (s == null || s.length() == 0) return false;
+        
+        int n = s.length();
+        
+        // dp[i] represents whether s[0...i] can be formed by dict
+        boolean[] dp = new boolean[n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                String sub = s.substring(j, i + 1);
+                
+                if (dict.contains(sub) && (j == 0 || dp[j - 1])) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[n - 1];
+    }
+}
