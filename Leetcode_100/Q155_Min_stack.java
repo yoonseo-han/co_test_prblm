216b20db36cd103e7fbe0f41bb9e69168cf39449
Yoonseo Han <yooncer00@gmail.com>
2023-03-28 16:07:50 +0800

    Leetcode Q155

---
+++ Leetcode_100/Q155_Min_stack.java
@@ -0,0 +1,47 @@
+package Leetcode_100;
+
+class Solution {
+    public static void main(String strs[]) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin()); // return -3
+        minStack.pop();
+        System.out.println(minStack.top());    // return 0
+        System.out.println(minStack.getMin()); // return -2
+    }
+}
+
+class MinStack {
+    Node head;
+    
+    public void push(int val) {
+        if(head==null) head = new Node(val, val, null);
+        else head = new Node(val, Math.min(head.min, val), head);
+    }
+    
+    public void pop() {
+        head = head.next;
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.min;
+    }
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
