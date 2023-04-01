6a42dc43e1a484d0e69f5506ea27bf961d1b5e9a
Yoonseo Han <yooncer00@gmail.com>
2023-04-01 15:09:59 +0800

    Leetcode recursion question

---
+++ Leetcode_100/Q231_Power_of_two.java
@@ -0,0 +1,13 @@
+package Leetcode_100;
+
+class Solution {
+    public static boolean isPowerOfTwo(int n) {
+        if(n==1) return true;
+        
+        return (n%2==0) && isPowerOfTwo(n/2);
+    }
+    public static void main(String strs[]) {
+        int inputNumber = 0;
+        System.out.println(isPowerOfTwo(inputNumber));
+    }
+}
