5f77534dc1b6ac3fba51123ed8150cdfaaa4adc6
Yoonseo Han <yooncer00@gmail.com>
2023-04-19 15:13:54 +0800

    Leetcode DP question

---
+++ Leetcode_100/Q279_Perfect_squares.java
@@ -0,0 +1,25 @@
+package Leetcode_100;
+
+import java.util.Vector;
+
+class Solution {
+    public static int numSquares(int n) {
+        if(n<=0) return 0;
+
+        int [] dp = new int[n+1];
+        dp[0] = 0;
+        for(int i=1; i<=n; i++) dp[i] = Integer.MAX_VALUE;
+
+        for(int i=1;i<=n; i++) {
+            for(int j=1; j*j<=i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j*j]+1);
+            }
+        }
+
+        return dp[n];
+    }
+    public static void main(String strs[]) {
+        int n = 12;
+        System.out.println(numSquares(n));
+    }
+}
