ca692fb58be4888616e80ba7ae7acf81079c6067
Yoonseo Han <yooncer00@gmail.com>
2023-05-16 19:40:33 +0800

    Leetcode Recursive approach

---
+++ Leetcode_100/Q504_Base7.java
@@ -0,0 +1,20 @@
+package Leetcode_100;
+
+class Solution {
+    public static String convertToBase7(int num) {
+        //Recursive approach
+
+        //For negative numbers
+        if (num < 0) return "-" + convertToBase7(-num);
+
+        //Concatenate result to string
+        if (num < 7) return Integer.toString(num);
+
+        //Concatenate latest result
+        return convertToBase7(num / 7) + Integer.toString(num % 7);
+    }
+    public static void main(String strs[]) {
+        int num = 100;
+        System.out.println(convertToBase7(num));
+    }
+}
