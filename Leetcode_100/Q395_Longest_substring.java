4461f6d8dd1892c83b1116e4dbe5191d94f01901
Yoonseo Han <yooncer00@gmail.com>
2023-07-02 15:32:40 +0800

    Leetcode Q395

--- Leetcode_100/Q395_Longest_substring.java
+++ Leetcode_100/Q395_Longest_substring.java
@@ -2,8 +2,30 @@ package Leetcode_100;
 
 class Solution {
     public static int longestSubstring(String s, int k) {
-        
-        return 0;
+        //Record frequency of each char
+        char[] chars = new char[26];
+        for (int i = 0; i < s.length(); i ++) chars[s.charAt(i) - 'a'] += 1;
+
+        boolean flag = true;
+        for (int i = 0; i < chars.length; i += 1) {
+            //Check if whole string contains any element that has frequency less than k
+            if (chars[i] < k && chars[i] > 0) flag = false;
+        }
+        // return the length of string if this string is a valid string
+        if (flag == true) return s.length();
+
+        int result = 0;
+        int start = 0, cur = 0;
+        // otherwise we use all the infrequent elements as splits
+        while(cur<s.length()) {
+            if (chars[s.charAt(cur) - 'a'] < k) {
+                result = Math.max(result, longestSubstring(s.substring(start, cur), k));
+                start = cur + 1;
+            }
+            cur++;
+        }
+        result = Math.max(result, longestSubstring(s.substring(start), k));
+        return result;
     }
     public static void main(String strs[]) {
         String s = "aaabb";
