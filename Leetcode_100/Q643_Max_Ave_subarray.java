20f783613516612ba9ad4f08cd8834af99c37685
Yoonseo Han <yooncer00@gmail.com>
2023-07-14 12:25:11 +0800

    Leetcode Q643

---
+++ Leetcode_100/Q643_Max_Ave_subarray.java
@@ -0,0 +1,28 @@
+package Leetcode_100;
+
+import java.util.Arrays;
+
+class Solution {
+    public static double findMaxAverage(int[] nums, int k) {
+        //Max subarray value: element values must be consecutive
+        
+        //Using the sliding window algorithm
+        
+        //First caclulate the first k sums
+        long sum = 0;
+        for (int i = 0; i < k; i++) sum += nums[i];
+        long max = sum;
+
+        //Move on and add on
+        for(int i=k; i<nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            max = Math.max(max, sum);
+        }
+
+        return max/k;
+    }
+    public static void main (String strs[]) {
+        int [] nums = {1,12,-5,-6,50,3};
+        System.out.println(findMaxAverage(nums, 4));
+    }
+}
