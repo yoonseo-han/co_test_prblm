947d049b818834c69c9ab70013937dc0dfdf02f8
Yoonseo Han <yooncer00@gmail.com>
2024-02-11 23:05:10 +0800

    Price of stock

--- Queue:Stack/PG_Paranthesis.java
+++ Queue:Stack/PG_Paranthesis.java
@@ -1,15 +1,27 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12909
-
-package Queue:Stack;
+//package Queue:Stack;
+import java.util.*;
 
 class Solution {
     public static boolean solution(String s) {
-        boolean answer = true;
 
-        return answer;
+        Stack<Character> tempStore = new Stack<>();
+
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(c == '(') tempStore.push('(');
+            else {
+                if(tempStore.isEmpty()) return false;
+                else tempStore.pop();
+            }
+        }
+
+        return (tempStore.isEmpty() == true) ? true:false ;
     }
 
     public static void main(String strs[]) {
-        
+        String s = "(()())(";
+        System.out.println(solution(s));
     }
 }
