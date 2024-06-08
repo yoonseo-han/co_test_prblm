ff4a9343795dfc74fe015cbc5c4f21d8a77c2c42
Yoonseo Han <yooncer00@gmail.com>
2024-06-08 17:45:49 +0900

    Determine prime number

---
+++ Boostcamp/K_Prime.java
@@ -0,0 +1,8 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/92335
+
+class Solution {
+    public int solution(int n, int k) {
+        int answer = -1;
+        return answer;
+    }
+}
