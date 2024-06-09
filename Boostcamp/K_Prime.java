4efb118caff4940eff613f2f618cdadf10cee201
Yoonseo Han <yooncer00@gmail.com>
2024-06-09 22:37:45 +0900

    HashMap implementation by removing duplicates

--- Boostcamp/K_Prime.java
+++ Boostcamp/K_Prime.java
@@ -1,8 +1,45 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/92335
 
+import java.util.*;
+
 class Solution {
+    public void getDecimal(int n, int k, Stack<Integer> tempStore) {
+        while(n/k != 0) {
+            tempStore.add(n%k);
+            n = n/k;
+        }
+        tempStore.add(n);
+    }
+    public boolean isPrime(long n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {			// n의 제곱근까지 나누기
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    
     public int solution(int n, int k) {
-        int answer = -1;
+        int answer = 0;
+        
+        Stack<Integer> tempStore = new Stack<Integer>();
+        getDecimal(n,k,tempStore);
+        Vector<Long> finalStore = new Vector<Long>();
+        
+        long curNum = 0;
+        while(!tempStore.isEmpty()) {
+            int getNum = tempStore.pop();
+            if(getNum == 0) {
+                if(curNum!=0) finalStore.add(curNum);
+                curNum = 0;
+            }
+            else curNum = (curNum*10) + getNum;
+        }
+        if(curNum!=0) finalStore.add(curNum);
+        
+        for(long i: finalStore) {
+            if(isPrime(i)) answer++;
+        }
+        
         return answer;
     }
 }
---
+++ Boostcamp/Result_announcement.java
@@ -0,0 +1,11 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=java
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+
+        
+
+        return answer;
+    }
+}
