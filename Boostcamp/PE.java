6471922db4b298cf6bb9320d0b38f2a40f478379
Yoonseo Han <yooncer00@gmail.com>
2024-06-11 17:32:59 +0900

    Backtracking

---
+++ Boostcamp/PE.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+import java.util.*;
+
+class Solution {
+    public static int backtracking(int[][] ability, boolean[] visited, int sportID, int curSum) {
+        if(sportID == ability[0].length) return curSum;
+        
+        int maxSum = curSum; // Use a local variable to keep track of the maximum su
+        for(int i=0; i<visited.length; i++) {
+            if(visited[i]) continue;
+            else {
+                visited[i] = true;
+                int newSum = backtracking(ability, visited, sportID + 1, curSum + ability[i][sportID]);
+                maxSum = Math.max(maxSum, newSum);
+                visited[i] = false;
+            }
+        }
+        return maxSum;
+    }
+    
+    public static int solution(int[][] ability) {
+        int answer = 0;
+        
+        // ArrayList<Person> curStore = new ArrayList<>();
+        // int curID = 0;
+        int numPpl = ability.length;
+        // for(int[] curArr : ability) {
+        //     Person curPerson = new Person(curID++);
+        //     for(int i = 0; i<numSport; i++) {
+        //         curPerson.sportLvl.add(curArr[i]);
+        //     }
+        // }
+        boolean[] visited = new boolean[numPpl];
+        answer = backtracking(ability, visited, 0, 0);
+        
+        
+        return answer;
+    }
+}
+
+class Person {
+    int id;
+    List<Integer> sportLvl;
+    public Person(int id) {
+        this.id = id;
+        sportLvl = new ArrayList<Integer>();
+    }
+}
--- Boostcamp/Result_announcement.java
+++ Boostcamp/Result_announcement.java
@@ -1,11 +1,32 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=java
 
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
-
+        int n = id_list.length;
+        int[] answer = new int[n];
+        //Get report ID and frequency\
+        HashMap<String, Integer> tempStore = new HashMap<>();
+        HashMap<String, Integer> storeReport = new HashMap<>();
         
-
+        //Remove duplicates
+        List<String> curListArr = Arrays.asList(report);
+        List<String> listWithoutDuplicates = new ArrayList<>(new HashSet<>(curListArr));
+        
+        for(String curString : listWithoutDuplicates) {
+            String [] tempStringArr = curString.split(" ");
+            tempStore.put(tempStringArr[1], tempStore.getOrDefault(tempStringArr[1], 0)+1);
+        }
+        for(String curString : listWithoutDuplicates) {
+            String [] tempStringArr = curString.split(" ");
+            if(tempStore.get(tempStringArr[1]) >= k) storeReport.put(tempStringArr[0], storeReport.getOrDefault(tempStringArr[0],0) + 1);
+        }
+        int i=0;
+        for(String curString: id_list) {
+            answer[i++] = storeReport.getOrDefault(curString, 0);
+        }
+        
         return answer;
     }
 }
