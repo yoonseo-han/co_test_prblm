53bc5ed81ac4f295a3259e582c17e4a549dbb261
Yoonseo Han <yooncer00@gmail.com>
2024-06-12 20:53:18 +0900

    Kakao blind

---
+++ Boostcamp/Key_and_lock.java
@@ -0,0 +1,8 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/60059
+
+class Solution {
+    public boolean solution(int[][] key, int[][] lock) {
+        boolean answer = true;
+        return answer;
+    }
+}
