c1f66e384bb3eb189c4266d0434d8ddb5d44ab23
Yoonseo Han <yooncer00@gmail.com>
2024-06-24 17:41:46 +0900

    Simple implementation from js

--- BFS_DFS/PG_Oil_extraction.java
+++ BFS_DFS/PG_Oil_extraction.java
@@ -43,6 +43,7 @@ class Solution {
         int N = land.length;
         int M = land[0].length;
         oilStore = new int[M];
+        visited = new boolean[N][M];
         
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
