b5cf97302646d4bea7ca63a92432a284fbffd516
Yoonseo Han <yooncer00@gmail.com>
2024-10-28 19:57:01 +0800

    Simple binary search Q

---
+++ Graph/PG_ranking.java
@@ -0,0 +1,34 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=java
+
+import java.util.*;
+
+class Graph {
+    private Map<Integer, List<Integer>> adjList;
+
+    public Graph() {
+        adjList = new HashMap<>();
+    }
+
+    public List containsNode(int nodeVal) {
+        for()
+    }
+}
+
+class Solution {
+    public static int solution(int n, int[][] results) {
+        int answer = 0;
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for(int[] curNode : results) {
+            
+        }
+
+        return answer;
+    }
+    public static void main(String strs[]) {
+        int n = 5;
+        int [][] results = {{4,3}, {4,2}, {3,2}, {1,2}, {2,5}};
+        System.out.println(solution(n, results));
+    }
+}
