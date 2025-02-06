1aafbc0a4890f7eac4af93e8b409739270fe1e80
Yoonseo Han <yooncer00@gmail.com>
2025-02-06 23:20:20 +0900

    BFS/DFS question related to graph

--- Graph/PG_ranking.java
+++ Graph/PG_ranking.java
@@ -2,33 +2,63 @@
 
 import java.util.*;
 
-class Graph {
-    private Map<Integer, List<Integer>> adjList;
-
-    public Graph() {
-        adjList = new HashMap<>();
-    }
-
-    public List containsNode(int nodeVal) {
-        for()
-    }
-}
-
 class Solution {
+    public static int BFS(int curNode, List<Integer>[] curGraph, int n) {
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> tempQueue = new LinkedList<>();
+        tempQueue.add(curNode);
+        visited[curNode] = true;
+
+        int ans = 0;
+
+        while (!tempQueue.isEmpty()) {
+            int tempNode = tempQueue.poll();
+            for (int child : curGraph[tempNode]) {
+                if (!visited[child]) {
+                    visited[child] = true;
+                    tempQueue.add(child);
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+
     public static int solution(int n, int[][] results) {
         int answer = 0;
+        List<Integer> winStore[] = new ArrayList[n + 1];
+        List<Integer> loseStore[] = new ArrayList[n + 1];
 
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i <= n; i++) {
+            winStore[i] = new ArrayList<>();
+            loseStore[i] = new ArrayList<>();
+        }
 
-        for(int[] curNode : results) {
-            
+        // Initialize graph
+        for (int[] curArr : results) {
+            int winPlayer = curArr[0];
+            int lostPlayer = curArr[1];
+
+            winStore[winPlayer].add(lostPlayer);
+            loseStore[lostPlayer].add(winPlayer);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int winCount = BFS(i, winStore, n);
+            int lostCount = BFS(i, loseStore, n);
+
+            if (winCount + lostCount == n - 1) {
+                answer++;
+            }
         }
 
         return answer;
     }
+
     public static void main(String strs[]) {
         int n = 5;
-        int [][] results = {{4,3}, {4,2}, {3,2}, {1,2}, {2,5}};
+        int[][] results = { { 4, 3 }, { 4, 2 }, { 3, 2 }, { 1, 2 }, { 2, 5 } };
         System.out.println(solution(n, results));
     }
 }
